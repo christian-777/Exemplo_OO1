cc0ec883be9e6badcf827aad7359fe848e5bf23e
christian-777 <christian.c.g.f777@hotmail.com>
2023-03-20 09:33:53 -0300

    Add project files.

---
+++ Exemplo_OO1/Pessoa.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo_OO1
+{
+    internal class Pessoa
+    {
+        string _nome;
+
+        public int _idade { get; }
+
+        public Pessoa(string nome) 
+        {
+            this._nome = nome;
+        }
+
+        public Pessoa() 
+        {
+            this._nome = "john doe";
+            this._idade = 0;
+            Console.WriteLine("A pessoa nasceu!!!");
+        }
+
+        public string getNome()
+        {
+            return this._nome;
+        }
+
+        public void setNome(string nome) 
+        {
+            this._nome = nome;
+        }
+    }
+}
---
+++ Exemplo_OO1/Program.cs
@@ -0,0 +1,13 @@
+﻿using Exemplo_OO1;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Pessoa pessoa = new Pessoa();
+
+        pessoa = new Pessoa("jose");
+        //pessoa.setNome("chris");
+        Console.WriteLine(pessoa.getNome());
+    }
+}
